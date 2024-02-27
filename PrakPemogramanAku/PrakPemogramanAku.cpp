33ed010d5c96201543bfb1287d3bf12a29934673
RizkyPratama05 <rizkypp428@gmail.com>
2024-02-27 18:50:15 +0700

    Add project files.

---
+++ PrakPemogramanAku/PrakPemogramanAku.cpp
@@ -0,0 +1,37 @@
+// PrakPemogramanAku.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	//numeric nJejari, nLuas
+	//display 'Masukan jejari= '
+	//accept nJejari
+	//compute nLuas = 3.14 * r * r
+	//display 'Luasnya= ' + nLuas
+	float nJejari, nLuas;
+	cout << " masukan jejari ";
+	cin >> nJejari;
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << " Luasnya = " << nLuas <<endl;
+
+
+
+	
+	
+}//end
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+}
+
