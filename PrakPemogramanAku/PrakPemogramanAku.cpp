b955bb008103c76bb8a572bbb0ad986bb79feade
RizkyPratama05 <rizkypp428@gmail.com>
2024-02-27 19:19:53 +0700

    memasukan variable

--- PrakPemogramanAku/PrakPemogramanAku.cpp
+++ PrakPemogramanAku/PrakPemogramanAku.cpp
@@ -12,6 +12,7 @@ int main()
 	//compute nLuas = 3.14 * r * r
 	//display 'Luasnya= ' + nLuas
 	float nJejari, nLuas;
+
 	cout << "=======Luas Lingkaran=======" << endl;
 	cout << " Masukan Jejari = ";
 	cin >> nJejari;
